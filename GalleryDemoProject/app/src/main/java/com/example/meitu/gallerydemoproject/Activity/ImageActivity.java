6bcc12088eef4b2d3fcf26338b547f1c64a0ff43
csm1 <csm1@meitu.com>
2017-07-12 15:49:28 +0800

    add 图片信息

---
+++ GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Activity/ImageActivity.java
@@ -0,0 +1,20 @@
+package com.example.meitu.gallerydemoproject.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.meitu.gallerydemoproject.R;
+
+/**
+ * Created by meitu on 2017/7/12.
+ */
+
+public class ImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.image_activity);
+
+    }
+}
