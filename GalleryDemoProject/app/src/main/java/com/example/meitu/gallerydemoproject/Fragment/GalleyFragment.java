2982bc0e91438bf5bc5abc9bc12d2a121577ec77
csm1 <csm1@meitu.com>
2017-07-13 19:18:30 +0800

    ADD 修改了代码风格，准备打注释，添加图片缩放功能，改UI

--- GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Fragment/GalleyFragment.java
+++ GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Fragment/GalleyFragment.java
@@ -14,6 +14,7 @@ import com.example.meitu.gallerydemoproject.Adapter.ImagesAdapter;
 import com.example.meitu.gallerydemoproject.R;
 import com.example.meitu.gallerydemoproject.Utils.AlbumsMessageUtils;
 
+
 import java.util.List;
 
 public class GalleyFragment extends Fragment {
