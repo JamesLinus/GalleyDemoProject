3a25b1bc9c6c4c016950b4ecfe6f6385faa386bc
csm1 <csm1@meitu.com>
2017-07-14 14:21:53 +0800

    ADD 图片显示页面的图片拉伸到顶住边框

--- GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Component/CustomImageView.java
+++ GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Component/CustomImageView.java
@@ -1,6 +1,7 @@
 package com.example.meitu.gallerydemoproject.Component;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
@@ -10,6 +11,13 @@ import android.widget.ImageView;
  */
 
 public class CustomImageView extends ImageView {
+
+    private int widthSize;
+    private int widthMode;
+    private int heightSize;
+    private int heithtMode;
+
+    private Drawable mContentDrawable;
 
     public CustomImageView(Context context) {
         this(context, null);
@@ -27,7 +35,12 @@ public class CustomImageView extends ImageView {
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        
+        setMeasuredDimension(widthMeasureSpec, heightMeasureSpec);
+        widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        widthMode = MeasureSpec.getMode(widthMeasureSpec);
+
+        heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        heithtMode = MeasureSpec.getMode(heightMeasureSpec);
     }
 
 }
---
+++ GalleryDemoProject/app/src/main/java/com/example/meitu/gallerydemoproject/Test/TestActivity.java
@@ -0,0 +1,17 @@
+package com.example.meitu.gallerydemoproject.Test;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by meitu on 2017/7/14.
+ */
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+}
